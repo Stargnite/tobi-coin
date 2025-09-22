7e7cdf4decc2c6c484378ced75820d76683ab3f6
Shobam <sobayotoby@gmail.com>
2025-09-23 00:24:33 +0100

    updated site

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        night: '#071224',
+        deep: '#0B1B34',
+        panel: '#0F2748',
+        fog: '#EAF2FF',
+        mute: '#9FB0C8',
+        lime: '#B7FF3C',
+        aqua: '#3FE7FF',
+        mag: '#FF5BC8',
+        sun: '#FFB561',
+      },
+      boxShadow: {
+        card: '0 18px 60px rgba(0,0,0,.55), inset 0 1px 0 rgba(255,255,255,.03)',
+      },
+    },
+  },
+};
